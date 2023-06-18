d8e3e44af82b072bba445930ae1f8912a0401cb9
Hayk Yolyan <54978371+Narberal-Gamma@users.noreply.github.com>
2023-06-18 10:55:20 +0400

    bug fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,20 +1,12 @@
 import axios from "axios";
 
-const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
 const apiUrl = 'http://a0830433.xsph.ru';
-const currentUrl = window.location.href;
 
 const $host = axios.create({
-    baseURL: corsProxyUrl + apiUrl,
+    baseURL: apiUrl,
     headers: {
         "Content-Type": "multipart/form-data",
     },
-});
-
-$host.interceptors.request.use(config => {
-    config.headers['Origin'] = currentUrl;
-    config.headers['X-Requested-With'] = 'XMLHttpRequest';
-    return config;
 });
 
 export { $host };
